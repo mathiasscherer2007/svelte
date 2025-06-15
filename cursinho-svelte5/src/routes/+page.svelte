8a5ed0631825fe0dc4cc4e0d89242bc4c169f4d2
Xerão <mathischerer@gmail.com>
2025-06-14 22:34:25 -0300

    sei lá

---
+++ cursinho-svelte5/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Header from "./Header.svelte";
+    let name = $state("");
+
+    let status: 'OPEN' | 'CLOSED' = $state("OPEN");
+    function toggle() {
+        status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+    }
+</script>
+
+<Header {name}/>
+<input type="text" bind:value={name}/>
+
+<p>The store is now {status}</p>
+<button onclick={toggle}>Toggle Status</button>
---
+++ cursinho-svelte5/src/routes/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { name } = $props();
+</script>
+
+<h1>{name} manda olá!</h1>
+<h2>{name.toUpperCase()}</h2>
+
+<style>
+
+</style>
