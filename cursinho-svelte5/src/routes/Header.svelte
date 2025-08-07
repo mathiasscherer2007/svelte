c37b729a0f8de3f33a39de9449f3e4ca4bbbd79c
Mathias <mathischerer@gmail.com>
2025-08-07 08:45:22 -0300

    idk

--- cursinho-svelte5/src/routes/Header.svelte
+++ cursinho-svelte5/src/routes/Header.svelte
@@ -1,21 +1,21 @@
 <!-- JS -->
 <script>
-    // Componentes começam com letra maiúscula
-    // Páginas começam com um +
+	// Componentes começam com letra maiúscula
+	// Páginas começam com um +
 
-    let { name, fake_name = null } = $props()
+	let { name, fake_name = null } = $props();
 </script>
 
 <!-- HTML -->
-<h1>Hello { name }</h1>
+<h1>Hello {name}</h1>
 
 <!-- CSS -->
- <style>
-    h1 {
-        color: red;
-    }
+<style>
+	h1 {
+		color: red;
+	}
 
-    *::before {
-        content: "> ";
-    }
- </style>
+	*::before {
+		content: '> ';
+	}
+</style>
