10bd44e6944d46c0be24c4e1c093f3cd56dfc865
Mathias <mathischerer@gmail.com>
2025-08-08 09:39:12 -0300

    Diminished 5th

---
+++ testes/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav>
+	<a href="./about">
+		<div class="navigation-container">bruh</div>
+	</a>
+</nav>
+
+{@render children?.()}
---
+++ testes/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+	<h1>Something</h1>
+</main>
+
+<style>
+</style>
